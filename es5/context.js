2e7b43c140c7f90cbb845d68c33e55087633783e
Adam Harvey <adam.harvey@dxc.com>
2018-01-31 16:45:05 -0500

    Add no color option

--- es5/context.js
+++ es5/context.js
@@ -66,7 +66,7 @@ function getLines(src, index, noBefore, noAfter) {
 }
 
 exports.default = {
-  getBlock: function getBlock(src, index, length) {
+  getBlock: function getBlock(src, index, length, options) {
     var lineInfo = getLines(src, index, 2, 2);
     var lineStart = 0;
     var lineEnd = lineInfo.line.length;
@@ -76,7 +76,13 @@ exports.default = {
     if (lineEnd - (lineInfo.column + length) > 30) {
       lineEnd = lineInfo.column + length + 30;
     }
-    var info = lineInfo.line.substring(lineStart, lineInfo.column) + _chalk2.default.red(lineInfo.line.substr(lineInfo.column, length)) + lineInfo.line.substring(lineInfo.column + length, lineEnd);
+
+    var info =  lineInfo.line.substring(lineStart, lineInfo.column) +
+                ( options.color ? "" : "[[[") +
+                _chalk2.default.red(lineInfo.line.substr(lineInfo.column, length)) +
+                ( options.color ? "" : "]]]") +
+                lineInfo.line.substring(lineInfo.column + length, lineEnd);
+
     return {
       info: info,
       lineNumber: lineInfo.lineNumber
