4a007ab69c6a7f489bcf39665f944545f61e68c3
Adam Harvey <adam.harvey@dxc.com>
2018-01-31 16:45:21 -0500

    Pass options to support no color

--- es5/report-generator.js
+++ es5/report-generator.js
@@ -35,12 +35,12 @@ function generateSummaryReport(results) {
 }
 
 // Generates a report for the errors found in a single markdown file.
-function generateFileReport(file, spellingInfo) {
+function generateFileReport(file, spellingInfo, options) {
   var report = '    ' + _chalk2.default.bold(file) + '\n';
 
   for (var k = 0; k < spellingInfo.errors.length; k++) {
     var error = spellingInfo.errors[k];
-    var displayBlock = _context2.default.getBlock(spellingInfo.src, error.index, error.word.length);
+    var displayBlock = _context2.default.getBlock(spellingInfo.src, error.index, error.word.length, options);
 
     var lineNumber = String(displayBlock.lineNumber);
     var lineNumberPadding = Array(10 - lineNumber.length).join(' ');
